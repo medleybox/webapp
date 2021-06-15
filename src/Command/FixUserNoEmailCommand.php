40433adae87e60d7e5de5eb029c93400af2c74c5
Chris Hilsdon <chrish@xigen.co.uk>
2021-06-15 03:07:57 +0100

    Added command to upgrade users that have no email set

---
+++ src/Command/FixUserNoEmailCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\{LocalUserRepository, MediaFileRepository};
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{InputArgument,InputInterface};
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class FixUserNoEmailCommand extends Command
+{
+    /**
+    * {@inheritdoc}
+    * @var string
+    */
+    protected static $defaultName = 'app:fix-user-email';
+
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var \App\Repository\MediaFileRepository
+     */
+    private $media;
+
+     /**
+     * @var \App\Repository\LocalUserRepository
+     */
+    private $users;
+
+    public function __construct(EntityManagerInterface $em, MediaFileRepository $media, LocalUserRepository $users)
+    {
+        $this->em = $em;
+        $this->media = $media;
+        $this->users = $users;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this->setDescription('Upgrade users that have no email set');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        foreach ($this->users->findAll() as $user) {
+            if (null !== $user->getEmail()) {
+                $user->setEmail('noreply@localhost');
+                $user->setActive(true);
+            }
+        }
+
+        $this->em->flush();
+        return 0;
+    }
+}
