ac7f7c06bf1e4fd2bcc8c0276a11b57966f1b053
AlphaRecon19 <git@ar19.uk>
2023-04-29 23:11:13 +0100

    Revert breaking changes in "Updating recipe for symfony/webpack-encore-bundle"
    This reverts parts of commit cbd8a72fda40eb578b57e4064073d016afb1707d.

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+
+import '../css/app.css';
+
+import 'material-icons/iconfont/material-icons.css';
+import '@fontsource/roboto/400.css';
+
+console.log('Medleybox!');
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ Encore
     /*
      * ENTRY CONFIG
      *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
